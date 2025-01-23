78755cd64de8c3068d2389ae6084f3b9b5bc1f03
hizkiia <109422084+hizkiia@users.noreply.github.com>
2025-01-24 00:54:01 +0700

    projects description

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -16,11 +16,13 @@
                 <div class="card-body">
                     <h4 class="card-title text-center">StayAwake</h4>
                     <p class="lead text-dark text-center">(Backend)</p>
-                    <p class="card-text text-justify">
-                        Android-based app designed to monitor drivers in real-time while driving. It uses Convolutional Neural Networks and Computer Vision methods to identify signs of inattention or abnormal conditions in the driver's body, such as fatigue or drowsiness.
-                    </p>
+                    <p style="font-size: 14px;" class="card-text text-justify">
+                        Android-based app designed to monitor drivers in real-time while driving.
+                        It uses Convolutional Neural Networks and Computer Vision methods to identify signs of inattention or abnormal conditions
+                        in the driver's body, such as fatigue or drowsiness. Upon detecting such indications, StayAwake will activate an alarm to
+                        increase the driver's alertness. Thus, this application plays an active role in preventing traffic accidents that can be
+                        caused by drowsiness while driving. I contributed as a backend developer using NodeJs with Express JS framework and MongoDB database. </p>
                     <div class="d-flex flex-column align-items-center justify-content-center">
-                        <a href="#" class="btn btn-primary btn-md mb-3">View More</a>
                         <a href="https://github.com/aldiiperdanaa/stayawake" class="btn btn-primary btn-md mb-3">Visit Repository</a>
                     </div>
                 </div>
@@ -34,11 +36,13 @@
                 <div class="card-body">
                     <h4 class="card-title text-center">EuforiaHome</h4>
                     <p class="lead text-dark text-center">(Backend)</p>
-                    <p class="card-text text-justify">
-                        Web-based platform designed to manage boarding house operations. Owners can manage billing, room bookings, payments, and other related tasks, while tenants can book rooms and access information.
-                    </p>
+                    <p style="font-size: 14px;" class="card-text text-justify">
+                        Web-based platform designed to manage boarding house operations.
+                        It has two main components: user and system. The user component includes boarding house owners and tenants.
+                        Owners can manage billing, room bookings, payments, and other related tasks, while tenants can book rooms, make payments,
+                        and access boarding house information. The system component consists of a database and a web application. The database stores
+                        all necessary information, and the web application provides an interface for user interaction. I contributed as a backend developer using PHP with the CodeIgniter 4 framework and MySQL database. </p>
                     <div class="d-flex flex-column align-items-center justify-content-center">
-                        <a href="#" class="btn btn-primary btn-md mb-3">View More</a>
                         <a href="https://github.com/hizkiia/ekost" class="btn btn-primary btn-md mb-3">Visit Repository</a>
                     </div>
                 </div>
@@ -52,14 +56,15 @@
                 <div class="card-body">
                     <h4 class="card-title text-center">Sudimoro Website</h4>
                     <p class="lead text-dark text-center">(Fullstack)</p>
-                    <p class="card-text text-justify">
-                        Web-based platform for showing and managing information about Dusun Sudimoro. Features include a gallery, news section, UMKM info, and a contact form.
-                    </p>
+                    <p style="font-size: 14px;" class="card-text text-justify">
+                        Web-based platform for showing and managing information about Dusun Sudimoro.
+                        Key features include a gallery showcasing local art, food, culture, and places;
+                        a news section for articles; UMKM information highlighting local businesses; and a contact form for inquiries.
+                        I made this website using CSS and JavaScript with Bootstrap 5 framework as a frontend, PHP with the CodeIgniter 4 framework as a backend and MySQL database. </p>
                     <p class="card-text text-center">
                         Website link: <a href="https://sudimoro.my.id">Click here</a>
                     </p>
                     <div class="d-flex flex-column align-items-center justify-content-center">
-                        <a href="#" class="btn btn-primary btn-md mb-3">View More</a>
                         <a href="https://github.com/hizkiia/website-sudimoro" class="btn btn-primary btn-md mb-3">Visit Repository</a>
                     </div>
                 </div>
